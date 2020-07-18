32274824cafb0409ddf06898d24b72d4a476d215
javierfurus <javierfurus@gmail.com>
2020-07-18 19:57:23 +0200

    Transferred data to mysql, includes a simple table generator with knex.

---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const handlebars = require('express-handlebars');
+const router = express.Router();
+// Knex options
+const options = {
+  client: 'mysql',
+  connection: {
+    host: 'localhost',
+    user: '****',
+    password: '*****',
+    database: '*****'
+  }
+};
+// Import Knex
+const knex = require('knex')(options);
+// Set up handlebars
+app.engine(
+  'hbs',
+  handlebars({
+    layoutsDir: path.join(__dirname, '/views/layouts'),
+    extname: 'hbs'
+  })
+);
+// Setup public directory
+app.use(express.static(path.join(__dirname, '/public')));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+// Routers
+router.get('/', function (req, res) {
+  // Read every row data in from cardContentFiller
+  knex
+    .from('cardContentFiller')
+    .select('*')
+    .then((rows) => {
+      console.log('Data acquired successfully!');
+      // Render index and use rows from cardContentFiller
+      res.render('index', { cardsContent: rows });
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+});
+
+router.get('/about', function (req, res) {
+  // Read every row data in from aboutCardContentFiller
+  knex
+    .from('aboutCardContentFiller')
+    .select('*')
+    .then((rows) => {
+      console.log('Data acquired successfully!');
+      // Render index and use rows from aboutCardContentFiller
+      res.render('about', { cardsContent: rows });
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+});
+
+router.get('/join', function (req, res) {
+  res.render('join');
+});
+app.use('/', router);
+app.listen(3000, function () {
+  console.log('Valley app listening on port 3000!');
+});
