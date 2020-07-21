a5dc49d60cec11edc6bfaac83bc9c12cb39180a6
javierfurus <javierfurus@gmail.com>
2020-07-21 15:58:16 +0200

    Unified routes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const handlebars = require('express-handlebars');
+const router = require('./routers/main');
 // Set up handlebars
 app.engine(
   'hbs',
@@ -15,18 +16,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 // Routers
-const indexRouter = require('./index');
-app.use('/index', indexRouter);
-
-const aboutRouter = require('./about');
-app.use('/about', aboutRouter);
-
-const joinRouter = require('./join');
-app.use('/join', joinRouter);
-
-app.get('/', function (req, res) {
-  res.redirect('/index');
-});
+app.use(router);
 app.listen(3000, function () {
   console.log('Valley app listening on port 3000!');
 });
